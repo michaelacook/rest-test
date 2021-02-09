5d8c4f376da311baf3c12e65fee9922e30f9c956
Michael A. Cook <mcook0775@gmail.com>
2021-02-09 18:01:37 -0500

    More fixes

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const createError = require("http-errors")
 const express = require("express")
 const path = require("path")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
-
-const indexRouter = require("./routes/index")
-const usersRouter = require("./routes/users")
+const cors = require("cors")
 
 const app = express()
 
