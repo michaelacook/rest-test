2b0dfcacd2df05728b474647c53a202eb67955c9
Michael A. Cook <mcook0775@gmail.com>
2021-02-09 18:39:34 -0500

    Try again

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const cors = require("cors")
 
 const app = express()
 
-// in production serve create-react-app index.html
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")))
-  app.get("/", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"))
-  })
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // view engine setup
