2ec34cc71f2a2e7120e1905ab340c90bf0d5cefe
Michael A. Cook <mcook0775@gmail.com>
2021-02-09 19:51:13 -0500

    Hopefully last fix

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-/* Build and deployment */
-app.use("/", express.static(path.join(__dirname, "./client/build")))
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build", "index.html"))
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("public"))
+  app.get("/", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+  })
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
